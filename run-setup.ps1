3a05f4cf8f7cc43f14749c2d5b5d70863aa0c4e6
johnkarlmolina <molina.johnkarl.ponteras@gmail.com>
2026-02-09 14:40:40 +0800

    2:40

---
+++ run-setup.ps1
@@ -0,0 +1,11 @@
+# Initialize project
+npm init -y
+
+# Install backend deps
+npm install express ejs mysql2 jquery
+
+# Install dev deps
+npm install -D tailwindcss postcss autoprefixer
+
+# For Alert Design sa mga gagawa ng backend
+npm install sweetalert2
