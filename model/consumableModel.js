6e211f03d7af38381014618eac01c49c3168d637
unknown <richardjamesgaspar23@gmail.com>
2026-02-24 12:18:21 +0800

    additional function to consumables form

--- model/consumableModel.js
+++ model/consumableModel.js
@@ -1,1 +1,7 @@
-//page lang
+const { query } = require("../config/db");
+
+exports.insertConsumables = async (item, item_classification, starting_stock, brand, model) => {
+    const sql = `INSERT INTO main_consumables (item, item_classification, starting_stock, brand, model) VALUES (?, ?, ?, ?, ?)`;  
+    const values = [item, item_classification, starting_stock, brand, model];
+    return query(sql, values);
+}
