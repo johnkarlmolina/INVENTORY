e97b7ac977ef60d783456e2d32b7c3f8b216bddb
johnkarlmolina <molina.johnkarl.ponteras@gmail.com>
2026-03-17 15:10:11 +0800

    add delete and edit function on user management data table, connect user-management to mysql database

--- route/userManagementRoute.js
+++ route/userManagementRoute.js
@@ -4,6 +4,9 @@ const router = express.Router();
 const userManagementController = require("../controller/userManagementController");
 
 router.get("/", userManagementController.renderUserManagement);
+router.post("/userDataTable", userManagementController.userDataTable);
 router.post("/addUser", userManagementController.addUser);
+router.post("/updateUser", userManagementController.updateUser);
+router.post("/deleteUser", userManagementController.deleteUser);
 
 module.exports = router;
