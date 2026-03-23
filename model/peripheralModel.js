9ec594656f6bf1064b20f202091692e0aa31901b
johnkarlmolina <molina.johnkarl.ponteras@gmail.com>
2026-03-23 13:34:22 +0800

    add inactive button on peripheras.ejs, add inactive peripherals data table, add activate button with function, add edit button function

--- model/peripheralModel.js
+++ model/peripheralModel.js
@@ -1,8 +1,10 @@
 const { query } = require("../config/db");
 
+const DELETED_STATUS = "inactive_deleted";
+
 exports.peripheralDataTable = async (req, res) => {
     try {
-        const peripheralsData = await query(`SELECT * FROM peripherals where peripheral_status != ?`, ['inactive']);
+        const peripheralsData = await query(`SELECT * FROM peripherals where COALESCE(LOWER(peripheral_status), '') != ?`, [DELETED_STATUS]);
         return peripheralsData;
     }   
     catch (error) {
@@ -11,14 +13,77 @@ exports.peripheralDataTable = async (req, res) => {
     }
 }
 
+exports.inactivePeripheralDataTable = async () => {
+    return query(`SELECT * FROM peripherals WHERE COALESCE(LOWER(peripheral_status), '') = ?`, [DELETED_STATUS]);
+}
+
 exports.addPeripheral = async (brand, model, date_of_purchase,  peripheral_user, user_dept, kind_of_peripheral, serial_no, property_tag, peripheral_no, peripheral_status, peripheral_location, computer_id) => { 
     //const { brand, model, date_of_purchase,  peripheral_user, user_dept, kind_of_peripheral, serial_no, property_tag, peripheral_no, peripheral_status, peripheral_location } = peripheralData;
+    const normalizedPeripheralStatus = typeof peripheral_status === 'string' ? peripheral_status.trim().toLowerCase() : null;
     const sql = `INSERT INTO peripherals (brand, model, date_of_purchase, peripheral_user, user_dept, kind_of_peripheral, serial_no, property_tag, peripheral_no, peripheral_status, peripheral_location, computer_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;  
-    const values = [brand, model, date_of_purchase,  peripheral_user, user_dept, kind_of_peripheral, serial_no, property_tag, peripheral_no, peripheral_status, peripheral_location, computer_id];
+    const values = [brand, model, date_of_purchase,  peripheral_user, user_dept, kind_of_peripheral, serial_no, property_tag, peripheral_no, normalizedPeripheralStatus, peripheral_location, computer_id];
+    return query(sql, values);
+}
+
+exports.updatePeripheral = async (peripheralData) => {
+    const {
+        peripheral_id,
+        brand,
+        model,
+        date_of_purchase,
+        peripheral_user,
+        user_dept,
+        kind_of_peripheral,
+        serial_no,
+        property_tag,
+        peripheral_no,
+        peripheral_status,
+        peripheral_location,
+        computer_id
+    } = peripheralData;
+
+    const normalizedPeripheralStatus = typeof peripheral_status === 'string' ? peripheral_status.trim().toLowerCase() : null;
+
+    const sql = `UPDATE peripherals
+                 SET brand = ?,
+                     model = ?,
+                     date_of_purchase = ?,
+                     peripheral_user = ?,
+                     user_dept = ?,
+                     kind_of_peripheral = ?,
+                     serial_no = ?,
+                     property_tag = ?,
+                     peripheral_no = ?,
+                     peripheral_status = ?,
+                     peripheral_location = ?,
+                     computer_id = ?
+                 WHERE peripheral_id = ?`;
+
+    const values = [
+        brand || null,
+        model || null,
+        date_of_purchase || null,
+        peripheral_user || null,
+        user_dept || null,
+        kind_of_peripheral || null,
+        serial_no || null,
+        property_tag || null,
+        peripheral_no || null,
+        normalizedPeripheralStatus,
+        peripheral_location || null,
+        computer_id || null,
+        peripheral_id
+    ];
+
     return query(sql, values);
 }
 
 exports.deletePeripheral = async (peripheral_id) => {
     const sql = `update peripherals set peripheral_status = ? WHERE peripheral_id = ?`;
-    return query(sql, ['inactive', peripheral_id]);
+    return query(sql, [DELETED_STATUS, peripheral_id]);
+}
+
+exports.activatePeripheral = async (peripheral_id) => {
+    const sql = `update peripherals set peripheral_status = ? WHERE peripheral_id = ?`;
+    return query(sql, ['active', peripheral_id]);
 }
--- route/peripheralRoute.js
+++ route/peripheralRoute.js
@@ -5,7 +5,10 @@ const peripheralController = require("../controller/peripheralController");
 // Peripherals Inventory route
 router.get("/peripheralPageRender", peripheralController.peripheralPageRender);
 router.post("/peripheralDataTable", peripheralController.peripheralDataTable);
+router.post("/inactivePeripheralDataTable", peripheralController.inactivePeripheralDataTable);
 router.post("/addPeripheral", peripheralController.addPeripheral);
+router.post("/updatePeripheral", peripheralController.updatePeripheral);
 router.post("/deletePeripheral", peripheralController.deletePeripheral);
+router.post("/activatePeripheral", peripheralController.activatePeripheral);
 
 module.exports = router;
